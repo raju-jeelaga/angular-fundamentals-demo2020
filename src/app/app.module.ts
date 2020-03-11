b880e23073e92477e79f59cd3a915e2178f2a0f9
Raju Jeelaga <rajujeelaga@gmail.com>
2020-03-11 23:11:22 +0530

    angular fundamental index page is ready and c-t m is added
    angular fundamental index page is ready and c-t m is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentsTemplatesModule } from './components-templates/components-templates.module'
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsTemplatesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
