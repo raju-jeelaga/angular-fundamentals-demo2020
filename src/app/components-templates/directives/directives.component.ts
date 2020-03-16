ea900520904de0114e52786f0617fac2d8ed8222
Raju Jeelaga <rajujeelaga@gmail.com>
2020-03-16 21:13:34 +0530

    structural directives are done
    structural directives are done

--- src/app/components-templates/directives/directives.component.ts
+++ src/app/components-templates/directives/directives.component.ts
@@ -16,7 +16,38 @@ export class DirectivesComponent implements OnInit {
 
   nullHero = null; 
 
+  heros = [{id:1,"name":"Raju",power:6352},
+  {id:2,"name":"Raju123",power:6362},
+  {id:3,"name":"RajuJ",power:6395},
+  {id:4,"name":"Jeelaga Raju",power:6622},
+  {id:5,"name":"Raju Jeelaga",power:5622}];
+
+  differentHeroNames = [{"id":1,"name":"Ram","power":2612},
+  {"id":2,"name":"Laxman","power":7036},
+  {"id":3,"name":"Barath","power":3331},
+  {"id":4,"name":"Shatrughna","power":5158},
+  {"id":5,"name":"Hanuman","power":8948}];
+
+  differentHeroIds = [{"id":11,"name":"Ram","power":2612},
+  {"id":12,"name":"Laxman","power":7036},
+  {"id":13,"name":"Barath","power":3331},
+  {"id":14,"name":"Shatrughna","power":5158},
+  {"id":15,"name":"Hanuman","power":8948}];
+
+
   ngOnInit(): void {
   }
+
+  trackByHeros(index:number, hero): number{
+    return hero.id;
+  }
+  updateWithDifferentNames(){
+    this.heros = this.differentHeroNames
+  }
+
+  updateWithDifferentIds(){
+    this.heros =  this.differentHeroIds
+  }
+
 
 }
